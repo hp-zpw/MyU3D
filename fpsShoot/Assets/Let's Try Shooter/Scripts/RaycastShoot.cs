539c69e4c8817db6742504d10b5391bf3c2fbdbb
zhongpengwei <peng-wei.zhong@successinfo.com.cn>
2017-06-23 14:59:05 +0800

    add source code

---
+++ fpsShoot/Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = .25f;
+    public float hitForce = 100f;
+
+    private Camera fpsCam;
+    private WaitForSeconds shotDuration = new WaitForSeconds(.07f);
+    private AudioSource gunAudio;
+    private LineRenderer laserLine;
+    private float nextFire;
+
+    void Start () {
+        laserLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        fpsCam = GetComponent<Camera>();
+
+	}
+	
+	void Update () {
+        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+        }
+	}
+
+    private IEnumerator ShotEffect()
+    {
+        gunAudio.Play();
+        laserLine.enabled = true;
+        yield return shotDuration;
+        laserLine.enabled = false;
+    }
+}
